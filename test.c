e2a462c6cd5a6a55d4861eb9c058905091fed22e
gushanshan <1141258345@qq.com>
2014-05-05 09:13:31 +0800

    this gushanshan

--- test.c
+++ test.c
@@ -1,35 +1,9 @@
 #include "student_info.h"
-<<<<<<< HEAD
-#include <stdio.h>
-=======
 #include <stdio.h>      
->>>>>>> 40cda09e518b6a37f33f8856e8e5940cc3a8f42c
 #include <stdlib.h>
 #include <time.h>
-
 int main()
-<<<<<<< HEAD
-{
-	int i,j;
-	for (int i=0;i<sizeof(info)/sizeof(struct student);++i) {
-		printf("%d-%s\n",info[i].id,info[i].name);
-	}
-	srand(time(NULL));
-	for(i=0;i<=39;i++){
-		j=rand()%40;
-		while(info[i]!=0)
-		  j++;
-		info[i]=i;
-	}
-	for(i=0;i<10;i++){
-		printf("第%d组：",i+1);
-		for(j=0;j<4;j++){
-			printf("%d-%s\n",info[i].id,info[i].name);
-			printf("\n");
-		}
-		return 0;
-	}
-=======
+	
 {	
 	
 	int i;int j=10;int flag=0;int count=0;
@@ -47,7 +21,7 @@ int main()
 	}
 	}
 	return 0;
->>>>>>> 40cda09e518b6a37f33f8856e8e5940cc3a8f42c
+
 }
 
 
