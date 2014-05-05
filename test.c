0006150bc9a71d60880d01f506a66ef3ae342589
zhangdongjun <929412918@qq.com>
2014-05-05 09:28:05 +0800

    zhangdngjun

--- test.c
+++ test.c
@@ -1,29 +1,30 @@
-#include "student_info.h"
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include "student_info.h"
 
-int main()
+void main()
 {
-	int i,j;
-	for (int i=0;i<sizeof(info)/sizeof(struct student);++i) {
-		printf("%d-%s\n",info[i].id,info[i].name);
-	}
-	srand(time(NULL));
-	for(i=0;i<=39;i++){
-		j=rand()%40;
-		while(info[i]!=0)
-		  j++;
-		info[i]=i;
-	}
-	for(i=0;i<10;i++){
-		printf("第%d组：",i+1);
-		for(j=0;j<4;j++){
-			printf("%d-%s\n",info[i].id,info[i].name);
-			printf("\n");
+ 	int group[10][4] = {0};
+ 	int i,j,k;
+ 	srand(time(NULL));
+ 	for(i = 0;i < 40;i++)
+ 	{
+  		j = rand() % 40; //生成0～39的随机数
+  		while(*(group[0] + j % 40) != 0)
+   		j++;
+  		*(group[0] + j % 40) = i;
+ 	}
+ 	for(i=0;i<10;i++)
+ 	{
+
+		printf("第%d组：\n",i+1);
+  		for(j=0;j<4;j++)
+		{
+			k=group[i][j];
+			printf("%d - %s\n", info[k].id, info[k].name);
 		}
-		return 0;
+   		/*printf("%d ",group[i][j]);*/
+  		printf("\n");
 	}
 }
-
-
