a48125c861bffde67ea073fc83a5448389f6cc44
lylnanjing <279369001@qq.com>
2014-05-05 09:32:37 +0800

    liyongling

--- test.c
+++ test.c
@@ -2,28 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-
-int main()
+void main()
 {
-	int i,j;
-	for (int i=0;i<sizeof(info)/sizeof(struct student);++i) {
-		printf("%d-%s\n",info[i].id,info[i].name);
+ 	int group[10][4]={0};
+ 	int i,j,k;
+        for (i = 0; i < sizeof(info)/sizeof(struct student); ++i) {
+		printf("%d - %s\n", info[i].id, info[i].name);
 	}
-	srand(time(NULL));
-	for(i=0;i<=39;i++){
-		j=rand()%40;
-		while(info[i]!=0)
-		  j++;
-		info[i]=i;
-	}
-	for(i=0;i<10;i++){
-		printf("第%d组：",i+1);
-		for(j=0;j<4;j++){
-			printf("%d-%s\n",info[i].id,info[i].name);
-			printf("\n");
+ 	srand(time(NULL));
+ 	for(i=0;i<40;i++)
+ 	{
+  		j=rand()%40;
+  		while(*(group[0]+j%40)!=0)
+   		j++;
+  		*(group[0]+j%40) = i;
+ 	}
+ 	for(i=0;i<10;i++)
+ 	{
+
+		printf("第%d组：\n",i+1);
+  		for(j=0;j<4;j++)
+		{
+			k=group[i][j];
+			printf("%d - %s\n", info[k].id, info[k].name);
 		}
-		return 0;
-	}
+  		printf("\n");
+ 	}
 }
-
-
